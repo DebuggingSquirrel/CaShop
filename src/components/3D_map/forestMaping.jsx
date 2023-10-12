f45bf61319fee8384d710d70f8056ac4823bb013
전해림 <HAERIM060920@gmail.com>
2023-10-13 03:20:52 +0900

    feat : ai

--- src/components/3D_map/forestMaping.jsx
+++ src/components/3D_map/forestMaping.jsx
@@ -1,12 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { extend, Canvas, useThree, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { ForestMap2 } from "../../assets/data/ForestMap2";
 import { Physics } from "@react-three/cannon";
-import React, { Suspense } from "react";
-import { DirectionalLight } from "three";
-import { Model } from "../../assets/data/NewCharacter";
+import React from "react";
+import { ForestMap2 } from "../../assets/data/ForestMap2";
+import * as THREE from "three";
+import test from "../../assets/image/test.jpg";
 
 extend({ OrbitControls });
 
@@ -23,10 +22,10 @@ const Controls = () => {
       ref={orbitRef}
       args={[camera, gl.domElement]}
       enableZoom={true}
-      dampingFactor={2.0} // 카메라 움직임의 감쇄를 조절
-      rotateSpeed={0.3} // 마우스 왼쪽 버튼을 클릭하고 드래그할 때 화면 회전 속도
-      panSpeed={5} // 마우스 오른쪽 버튼을 드래그하여 화면을 패닝(이동)할 때의 속도
-      zoomSpeed={1} // 마우스 휠로 화면 확대/축소
+      dampingFactor={2.0}
+      rotateSpeed={0.3}
+      panSpeed={5}
+      zoomSpeed={1}
     />
   );
 };
@@ -42,20 +41,97 @@ const CameraSetup = () => {
   return null;
 };
 
-const MapingPage = () => {
+function Character() {
+  const [position, setPosition] = useState([19, 0, 0]);
+  const moveSpeed = 0.1;
+
+  const faceTexture = new THREE.TextureLoader().load(test);
+
+  const handleKeyDown = (event) => {
+    let x = position[0];
+    let z = position[2];
+
+    switch (event.key) {
+      case "ArrowUp":
+        z += moveSpeed;
+        break;
+      case "ArrowDown":
+        z -= moveSpeed;
+        break;
+      case "ArrowLeft":
+        x -= moveSpeed;
+        break;
+      case "ArrowRight":
+        x += moveSpeed;
+        break;
+      default:
+        break;
+    }
+
+    setPosition([x, 0, z]);
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [position]);
+
+  return (
+    <group position={position}>
+      {/* 머리 */}
+      <mesh position={[0, 4, 0]} rotation={[0, Math.PI / 2, 0]}>
+        <sphereGeometry args={[1, 10, 10]} />
+        <meshBasicMaterial map={faceTexture} />
+      </mesh>
+
+      {/* 몸통 */}
+      <mesh position={[0, 3, 0]}>
+        <boxGeometry args={[1, 2, 0.5]} />
+        <meshBasicMaterial color={0x336699} />
+      </mesh>
+
+      {/* 왼팔 */}
+      <mesh position={[-1, 3, 0]}>
+        <boxGeometry args={[0.3, 1, 0.3]} />
+        <meshBasicMaterial color={0xff0000} />
+      </mesh>
+
+      {/* 오른팔 */}
+      <mesh position={[1, 3, 0]}>
+        <boxGeometry args={[0.3, 1, 0.3]} />
+        <meshBasicMaterial color={0xff0000} />
+      </mesh>
+
+      {/* 왼다리 */}
+      <mesh position={[-0.5, 1.5, 0]}>
+        <boxGeometry args={[0.3, 1, 0.3]} />
+        <meshBasicMaterial color={0x0000ff} />
+      </mesh>
+
+      {/* 오른다리 */}
+      <mesh position={[0.5, 1.5, 0]}>
+        <boxGeometry args={[0.3, 1, 0.3]} />
+        <meshBasicMaterial color={0x0000ff} />
+      </mesh>
+    </group>
+  );
+}
+
+function App() {
   return (
     <Canvas style={{ width: "100vw", height: "100vh" }}>
-      <directionalLight intensity={2} position={[1, 20, 5]} />
+      <directionalLight intensity={2} position={[-1, 10, 1]} />
       <CameraSetup />
-      <Suspense fallback={null}>
-        <Physics>
-          <Model />
-          <ForestMap2 />
-        </Physics>
-        <Controls />
-      </Suspense>
+      <Controls />
+      <Physics>
+        <ForestMap2 />
+        <Character />
+      </Physics>
     </Canvas>
   );
-};
+}
 
-export default MapingPage;
+export default App;
